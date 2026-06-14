3b68158f61e3bd5ded0abc840e357ed437c9a172
Luciano Lagoria <Luciano.LagoriaVillagran@alu.frt.utn.edu.ar>
2026-06-14 16:10:07 -0300

    refactor(services): extract admin users module

--- src/services/api.js
+++ src/services/api.js
@@ -11,10 +11,7 @@ export { createPaymentPreference, getPaymentSettings } from './payments';
 // Admin - Statistics
 export const getAdminStats = () => api.get('/admin/stats');
 
-// Admin - Users
-export const getAdminUsers = (params = {}) => api.get('/admin/users', { params });
-export const updateUserRole = (userId, newRole) =>
-    api.put(`/admin/users/${userId}/role`, null, { params: { new_role: newRole } });
+export { getAdminUsers, updateUserRole } from './admin/adminUsers';
 
 // Admin - Orders
 export const getAdminOrders = (params = {}) => api.get('/admin/orders', { params });
